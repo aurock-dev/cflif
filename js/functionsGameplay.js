2a1653005459a7cf7e0d9234d1307f22bf9a612a
aurock <jeremy.dussol@epsi.fr>
2021-12-14 15:14:15 +0100

    ✨ fight de monstre ok, un seul fight à la fois

--- js/functionsGameplay.js
+++ js/functionsGameplay.js
@@ -1,17 +1,4 @@
 function attack(index){
-    // monsters["monster"+index]["hp"] = hpMonstre - atk;
-    // document.querySelector("#hpm").innerHTML = hpMonstre;
-    // if (hpMonstre <= 0){
-    //     hpMonstre = 10;
-    //     exp += 1;
-    //     document.querySelector("#exp").innerHTML = exp;
-    //     if (exp == calculExp(lvl)){
-    //         lvl += 1;
-    //         exp = 0
-    //         atk += 1;
-    //         displayStats();
-    //     }
-    // }
     var monsterIndex = "monster"+index
     var queryHpm = "#hpm"+index;
     monsters[monsterIndex]["hp"] = monsters[monsterIndex]["hp"] - atk;
@@ -27,11 +14,16 @@ function attack(index){
             displayStats();
         }
     }
-
 }
 
+var toggleFight;
+
 function fight(index){
-    window.setInterval(function(){
-        attack(index+1);
-    }, vitesseAtk);
+    if (!toggleFight){
+        toggleFight = setInterval(function(){attack(index+1);}, vitesseAtk);
+    }
+    else if(toggleFight){
+        clearInterval(toggleFight);
+        toggleFight = null;
+    }
 }
