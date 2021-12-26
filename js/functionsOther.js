3bd96f962efa10e68e215a6ca8f7acc808086582
aurock <jeremy.dussol@epsi.fr>
2021-12-26 22:41:42 +0100

    🚧 add progress bar for player exp

--- js/functionsOther.js
+++ js/functionsOther.js
@@ -10,6 +10,6 @@ function subPercentage(stat, multiplicator){
     return Math.round(stat - (multiplicator/100*stat));
 }
 
-function calcPercentage(val1, val2){
-    return (val1*100)/val2;
+function calcPercentage(remainValue, maxValue){
+    return (remainValue*100)/maxValue;
 }
