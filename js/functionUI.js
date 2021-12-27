30eb18952bae41fe65d7c554d4c305df162f9d1f
aurock <jeremy.dussol@epsi.fr>
2021-12-27 15:27:56 +0100

    🚧 some change on UI design for monster list

--- js/functionUI.js
+++ js/functionUI.js
@@ -28,6 +28,7 @@ function displayMonsters(){
         var monsterHPMax = monsters[key]["hpMax"];
         var monsterAtk = monsters[key]["atk"];
         var monsterAtkSpeed = "every " + monsters[key]["atkSpeed"]/1000 + " sec";
+        var monsterGold = monsters[key]["gold"];
         var buttonId = "fightButton"+cpt;
         var progressId = "monsterHPPB"+cpt;
         var hpmId = "hpm"+cpt;
@@ -35,15 +36,18 @@ function displayMonsters(){
         
         $('#monsterList').append('<div class="row align-items-center top-buffer" id='+monsterRow+'></div>');
 
+        var monsterButton = '<div class="col-2 text-center "><button type="button" id='+buttonId+' class="btn btn-outline-primary btn-block">'+fightText+'</button></div>';
+        
+        var monsterName = '<div class="col-sm text-center" id="detailMonster">'+monsterName+' | Level : '+monsterLvl+' | Exp given : '+monsterExp+' | Gold dropped : '+monsterGold+'</div>';
+        
         var monsterHPPB = '<div class="progress top-buffer" style="height: 15px;">'+
         '<div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" id='+progressId+' role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">'+
         '<div class="progress-bar-text"><nobr>HP : <span id='+hpmId+'>'+monsterHP+'</span> / '+monsterHPMax+'</nobr></div>'+
         '</div>'+
         '</div>'
-
-        var monsterButton = '<div class="col- "><button type="button" id='+buttonId+' class="btn btn-outline-primary">'+fightText+'</button></div>';
-        var monsterName = '<div class="col-sm" id="detailMonster">'+monsterName+' | Level : '+monsterLvl+' | Exp given : '+monsterExp+'</div>';
-        var monsterDetail = '<div class="row"><div class="col-sm">'+monsterHPPB+'</div><div class="col-sm"> Atk : '+monsterAtk+' | Atk Speed : '+monsterAtkSpeed+'</div><div class="col-sm"></div></div>';
+        
+        var monsterDetail = '<div class="row text-center"><div class="col-sm">'+monsterHPPB+'</div><div class="col-sm"> Atk : '+monsterAtk+' | Atk Speed : '+monsterAtkSpeed+'</div></div>';
+        
         $('#'+monsterRow).append(monsterButton+'<div class="col-sm border border-primary">'+monsterName+monsterDetail+'</div>');
         cpt += 1;
     }
