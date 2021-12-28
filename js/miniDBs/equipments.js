4a011acaa69a0784e7540eeade0210c4f8e01f09
aurock <jeremy.dussol@epsi.fr>
2021-12-28 12:21:34 +0100

    🚧 add more stuff to db

--- js/miniDBs/equipments.js
+++ js/miniDBs/equipments.js
@@ -12,10 +12,44 @@ var weapon = {
         type : "weapon",
         name : "bow",
         damage : [6,9],
-        bonuses : 2,
         bonusStats : {
             "criticalChance" : [1,5],
             "agility" : [1,3]
+        }
+    }
+}
+
+var helmet = {
+    h1 : {
+        type : "helmet",
+        name : "helmet",
+        defense : [1,3],
+        bonusStats : {
+            "vigour" : [2,4]
+        }
+    }
+}
+
+var chest = {
+    c1 : {
+        type : "chest",
+        name : "chest",
+        defense : [3,6],
+        bonusStats : {
+            "attack" : [5,9],
+            "force" : [3,8]
+        }
+    }
+}
+
+var boots = {
+    b1 : {
+        type : "boots",
+        name : "boots",
+        defense : [2,4],
+        bonusStats : {
+            "expBonus" : [1,6],
+            "criticalChance" : [1,4]
         }
     }
 }
