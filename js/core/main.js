cef7f2c10fced842d2e5f63a88d9ae4df169a3fd
aurock <jeremy24200@gmail.com>
2022-03-13 15:12:37 +0100

    💄 using bs tooltip for instant display

--- js/core/main.js
+++ js/core/main.js
@@ -5,6 +5,11 @@ function main(){
     selectAll();
     displayUpgradableStat(true);
     saveButton();
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 }
 
 main();
