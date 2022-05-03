226acdd095560850b547abf6914533d03e1994c1
aurock <jeremy.dussol@epsi.fr>
2022-05-03 16:21:50 +0200

    💄 last changes

--- design/toast.js
+++ design/toast.js
@@ -1,10 +1,4 @@
 function toastAction(text, color){
-    $('.messagesBox').css('opacity', '1');
     $('.messagesBox').css('background-color', color);
     $('.messagesBox').text(text);
-    toast = setTimeout(function(){
-        $('.messagesBox').fadeTo( 200, 0, function() {});
-    }, 400);
-
-
 }
